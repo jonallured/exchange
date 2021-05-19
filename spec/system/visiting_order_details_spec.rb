c3a49713829852741efe2cf7d6363403326b4638
Chung-Yi Chi <chung-yi@artsymail.com>
2021-05-19 15:47:25 -0400

    feat: link inquiry orders to impulse conversation in admin

--- spec/system/visiting_order_details_spec.rb
+++ spec/system/visiting_order_details_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'visit order details', type: :system do
-  let!(:order) { Fabricate(:order) }
+  let(:order) { Fabricate(:order) }
   before do
     stub_request(:get, "#{Rails.application.config_for(:gravity)[:api_v1_root]}/user/#{order.buyer_id}")
       .to_return(status: 200, body: {}.to_json, headers: {})
 
     stub_request(:get, "#{Rails.application.config_for(:gravity)[:api_v1_root]}/partner/#{order.seller_id}/all")
       .to_return(status: 200, body: {}.to_json, headers: {})
+    allow_any_instance_of(ApplicationController).to receive(:require_artsy_authentication)
   end
 
   it 'renders order details page', js: true do
-    allow_any_instance_of(ApplicationController).to receive(:require_artsy_authentication)
     visit '/admin'
 
     page_title_selector = 'h2#page_title'
@@ -35,6 +35,15 @@ RSpec.describe 'visit order details', type: :system do
 
     within page_title_selector do
       expect(page).to have_content("Order #{order.id}")
+    end
+  end
+
+  context 'for an inquiry order' do
+    let(:order) { Fabricate(:order, impulse_conversation_id: '123') }
+
+    it 'renders a link to Impulse conversation' do
+      visit "/admin/orders/#{order.id}"
+      expect(page).to have_link('123', href: 'https://impulse-staging.artsy.net/admin/conversations/123')
     end
   end
 end
